a406acdd86cdc6a30b9499136a9df7eda924b304
ImEtienne <graddykibongani@gmail.com>
2024-11-03 19:11:59 +0100

    debug

--- src/app/component/job-list/job-list.component.ts
+++ src/app/component/job-list/job-list.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { MockDataService } from '../../services/mock-data.service';
-import { CommonModule } from '@angular/common'; // Ajoute cette ligne
+import { CommonModule } from '@angular/common'; 
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-job-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './job-list.component.html',
-  styleUrl: './job-list.component.scss'
+  styleUrls: ['./job-list.component.scss'] 
 })
 export class JobListComponent {
 
@@ -16,6 +17,10 @@ export class JobListComponent {
   constructor(private mockDataService: MockDataService) {}
 
   ngOnInit(): void {
+    this.loadData();
+  }
+
+  loadData() {
     this.mockDataService.getJobListings().subscribe(data => {
       this.jobListings = data;
     });
--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -4,14 +4,18 @@ import { FooterComponent } from '../../component/footer/footer.component';
 import { JobListComponent } from '../../component/job-list/job-list.component';
 import { CommonModule } from '@angular/common'; // Ajoute cette ligne
 import { HttpClientModule } from '@angular/common/http'; // Ajoute cette ligne
+import { MockDataService } from '../../services/mock-data.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HttpClientModule,CommonModule,HeaderComponent,FooterComponent,JobListComponent],
+  imports: [HttpClientModule, CommonModule, HeaderComponent, FooterComponent, JobListComponent],
+  providers: [MockDataService], 
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
+  
+
 export class HomeComponent {
 
 }
