74d446cc40caef06dde42bf0d75eb75027ded4b8
ImEtienne <graddykibongani@gmail.com>
2024-11-09 20:39:47 +0100

    :wrench: job-list

--- src/app/page/jobs/jobs.component.ts
+++ src/app/page/jobs/jobs.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { JobListComponent } from "../../component/job-list/job-list.component";
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common'; 
+import { MockDataService } from '../../services/mock-data.service';  
 
 @Component({
   selector: 'app-jobs',
   standalone: true,
-  imports: [],
+  imports: [
+    HttpClientModule,
+    CommonModule,
+    JobListComponent
+],
+  providers: [MockDataService], 
   templateUrl: './jobs.component.html',
   styleUrl: './jobs.component.scss'
 })
 export class JobsComponent {
-
+  
 }
