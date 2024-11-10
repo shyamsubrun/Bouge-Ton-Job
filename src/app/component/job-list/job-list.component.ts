3423de1f70585845a4396ada6bf0e16ddedadbfd
shyam <shyam1@live.fr>
2024-11-10 12:20:30 +0100

    component searchfilter & et joblist

--- src/app/component/job-list/job-list.component.ts
+++ src/app/component/job-list/job-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { MockDataService } from '../../services/mock-data.service';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
@@ -13,28 +13,11 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./job-list.component.scss'],
   providers: [MockDataService]
 })
-export class JobListComponent implements OnInit {
+export class JobListComponent {
 
-  jobListings: any[] = [];
-  filteredJobListings: any[] = [];
+  @Input() jobListings: any[] = [];
 
-  searchTitle: string = '';
-  searchLocation: string = '';
-  employmentType: string = '';
-  remoteOnly: boolean = false;
-
-  constructor(private mockDataService: MockDataService, private router: Router) {}
-
-  ngOnInit(): void {
-    this.loadJobs(); // Charger tous les jobs dès l'initialisation
-  }
-
-  loadJobs(): void {
-    this.mockDataService.getJobListings().subscribe(data => {
-      this.jobListings = data;
-      this.filteredJobListings = [...this.jobListings]; // Initialiser filteredJobListings avec tous les jobs
-    });
-  }
+  constructor(private router: Router) {}
 
   getDaysAgo(dateString: string): number {
     const publishedDate = new Date(dateString);
@@ -45,19 +28,5 @@ export class JobListComponent implements OnInit {
 
   goToJobDetail(id: number): void {
     this.router.navigate(['/jobs', id]);
-  }
-
-  applyFilters(): void {
-    const titleQuery = this.searchTitle.toLowerCase();
-    const locationQuery = this.searchLocation.toLowerCase();
-
-    this.filteredJobListings = this.jobListings.filter(job => {
-      const matchesTitle = titleQuery ? job.title.toLowerCase().includes(titleQuery) : true;
-      const matchesLocation = locationQuery ? job.location.toLowerCase().includes(locationQuery) : true;
-      const matchesType = this.employmentType ? job.employment_type === this.employmentType : true;
-      const matchesRemote = this.remoteOnly ? job.remote : true;
-
-      return matchesTitle && matchesLocation && matchesType && matchesRemote;
-    });
   }
 }
--- src/app/page/jobs/jobs.component.ts
+++ src/app/page/jobs/jobs.component.ts
@@ -3,6 +3,7 @@ import { JobListComponent } from "../../component/job-list/job-list.component";
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common'; 
 import { MockDataService } from '../../services/mock-data.service';  
+import { SearchFiltersComponent } from '../../component/search-filters/search-filters.component';
 
 @Component({
   selector: 'app-jobs',
@@ -10,12 +11,41 @@ import { MockDataService } from '../../services/mock-data.service';
   imports: [
     HttpClientModule,
     CommonModule,
-    JobListComponent
+    JobListComponent,SearchFiltersComponent
 ],
-  providers: [MockDataService], 
+  providers: [SearchFiltersComponent,MockDataService], 
   templateUrl: './jobs.component.html',
   styleUrl: './jobs.component.scss'
 })
-export class JobsComponent {
-  
+export class JobsComponent  {
+
+  jobListings: any[] = [];
+  filteredJobListings: any[] = [];
+
+  constructor(private mockDataService: MockDataService) {}
+
+  ngOnInit(): void {
+    this.loadJobs();
+  }
+
+  loadJobs(): void {
+    this.mockDataService.getJobListings().subscribe(data => {
+      this.jobListings = data;
+      this.filteredJobListings = [...this.jobListings];
+    });
+  }
+
+  onFiltersChanged(filters: any): void {
+    const titleQuery = filters.title.toLowerCase();
+    const locationQuery = filters.location.toLowerCase();
+
+    this.filteredJobListings = this.jobListings.filter(job => {
+      const matchesTitle = titleQuery ? job.title.toLowerCase().includes(titleQuery) : true;
+      const matchesLocation = locationQuery ? job.location.toLowerCase().includes(locationQuery) : true;
+      const matchesType = filters.type ? job.employment_type === filters.type : true;
+      const matchesRemote = filters.remoteOnly ? job.remote : true;
+
+      return matchesTitle && matchesLocation && matchesType && matchesRemote;
+    });
+  }
 }
